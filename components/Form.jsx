852e2044d1cc02f930c9f242619b6d1a62ace896
Beboo-stack <peteralber735@gmail.com>
2024-05-06 20:12:21 +0300

    finish ui

---
+++ components/Form.jsx
@@ -0,0 +1,203 @@
+"use client";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import React from "react";
+import { useState, useRef } from "react";
+
+import { z } from "zod";
+
+const Form = () => {
+  const router = useRouter();
+
+  const [formData, setFormData] = useState({
+    name: "",
+    phone: "",
+    quantity: 1,
+    address: "",
+    total: 250,
+  });
+
+  const schema = z.object({
+    name: z.string().nonempty().min(3),
+    phone: z.string().min(11),
+    address: z.string().min(10),
+    quantity: z.number().min(1),
+    total: z.number().min(1),
+  });
+
+  const Increase = () => {
+    setFormData((prev) => ({
+      ...prev,
+      quantity: prev.quantity + 1,
+      total: 250 * (prev.quantity + 1),
+    }));
+  };
+
+  const Decrease = () => {
+    if (formData.quantity >= 2) {
+      setFormData((prev) => ({
+        ...prev,
+        quantity: prev.quantity - 1,
+        total: 250 * (prev.quantity - 1),
+      }));
+    }
+  };
+
+  const handleChange = (event) => {
+    setFormData({ ...formData, [event.target.name]: event.target.value });
+  };
+
+  const handleSelectedOption = (event) => {
+    const selectedOption = event.target.value;
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      selectedOption: selectedOption,
+    }));
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const validatedData = schema.parse(formData);
+
+      const seviceId = process.env.NEXT_PUBLIC_SERVICE_ID;
+      const templateID = process.env.NEXT_PUBLIC_TEMPLATE_ID;
+      const publicKey = process.env.NEXT_PUBLIC_PUBLIC_KEY;
+
+      const templateParams = {
+        from_name: formData.name,
+        to_name: "johnathan",
+        phone: formData.phone,
+        address: formData.address,
+        quantity: formData.quantity,
+        total: formData.total,
+      };
+
+      if (!formData.name || !formData.phone) return;
+
+      emailjs
+        .send(seviceId, templateID, templateParams, publicKey)
+        .then(() => {
+          setFormData({
+            name: "",
+            phone: "",
+            address: "",
+            total: 250,
+            quantity: 1,
+          });
+          router.push("/successfull");
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    } catch (error) {
+      console.error("Form validation failed:", error.errors);
+    }
+  };
+
+  return (
+    <section id="contact" className="">
+      <div className="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8 ">
+        <div className="grid grid-cols-1 md:grid-cols-1 gap-x-16 gap-y-8 ">
+          <div className="rounded-[10px] bg-white p-8  shadow-lg  lg:p-12 ">
+            <h2 className="text-3xl font-bold pt-8 mb-8 text-center">
+              Order Now
+            </h2>
+            <form onSubmit={handleSubmit} className="space-y-4">
+              <div className="flex justify-between items-center overflow-hidden">
+                <div className="flex flex-col justify-center items-center">
+                  <h2 className="text-lg">عدد القطع</h2>
+                  <div className="flex justify-start items-center gap-5">
+                    <button
+                      type="button"
+                      className="cursor-pointer text-[30px] text-center self-center w-[30px]"
+                      onClick={Decrease}
+                    >
+                      -
+                    </button>
+                    <h2 className="text-lg">{formData.quantity} </h2>
+                    <button
+                      type="button"
+                      className="w-[30px] cursor-pointer text-[30px]   "
+                      onClick={Increase}
+                    >
+                      +
+                    </button>
+                  </div>
+                </div>
+                <div className="flex flex-col justify-center items-center">
+                  <h2 className="text-xl">السعر النهائي</h2>
+                  <p className="text-[20px] font-bold">
+                    {" "}
+                    {formData.total} <span>EGP</span>{" "}
+                  </p>
+                </div>
+              </div>
+              <div>
+                <label className="sr-only" htmlFor="name">
+                  Name
+                </label>
+                <input
+                  className="w-full rounded-lg border-gray-200 p-3 text-sm"
+                  placeholder="الاسم"
+                  type="text"
+                  name="name"
+                  value={formData.name}
+                  onChange={handleChange}
+                  required
+                  id="name"
+                />
+              </div>
+              <div className="grid grid-cols-1 gap-4">
+                <div>
+                  <label className="sr-only" htmlFor="phone">
+                    Phone
+                  </label>
+                  <input
+                    className="w-full rounded-lg border-gray-200 p-3 text-sm"
+                    placeholder="رقم التليفون"
+                    type="tel"
+                    onChange={handleChange}
+                    name="phone"
+                    value={formData.phone}
+                    required
+                    id="phone"
+                  />
+                </div>
+              </div>
+
+              <div>
+                <label className="sr-only" htmlFor="address">
+                  Address
+                </label>
+
+                <textarea
+                  className="w-full rounded-lg border-gray-200 p-3 text-sm"
+                  required
+                  placeholder="العنوان بالكامل "
+                  rows="8"
+                  name="address"
+                  onChange={handleChange}
+                  value={formData.address}
+                  id="address"
+                ></textarea>
+              </div>
+
+              <div className="mt-4">
+                <button
+                  type="submit"
+                  className=" text-2xl text-center my-8 text-white bg-black py-3 rounded-xl w-full"
+                >
+                  اضغط هنا للشراء
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Form;
--- components/Product.jsx
+++ components/Product.jsx
@@ -3,6 +3,10 @@ import { Repeat2, Share2 } from "lucide-react";
 import Image from "next/image";
 import { useEffect } from "react";
 import { useState } from "react";
+import { useRouter } from "next/router";
+import { emailjs } from "emailjs-com";
+import Form from "./Form";
+import Link from "next/link";
 
 const Product = () => {
   const COUNTDOWN_TARGET = new Date("2024-05-09T00:00:00").getTime(); // Replace with your target date
@@ -27,37 +31,87 @@ const Product = () => {
     };
   });
 
-  const data = [
+  // const router = useRouter();
+
+  const [formData, setFormData] = useState({
+    name: "",
+    phone: "",
+    quantity: 1,
+    address: "",
+    total: 7600,
+    selectedOption: "دفع عند الاستلام",
+  });
+
+  const images = [
+    "/1.jpeg",
+    "/2.jpeg",
+    "/3.jpeg",
+    "/4.jpeg",
+    "/5.jpeg",
+    "/6.jpeg",
+  ];
+
+  const images1 = [
     {
-      img: "/1.jpeg",
+      category: "gray",
+      src: "/1.jpeg",
     },
     {
-      img: "/2.jpeg",
+      category: "gray",
+      src: "/2.jpeg",
     },
     {
-      img: "/3.jpeg",
+      category: "gray",
+      src: "/3.jpeg",
     },
     {
-      img: "/4.jpeg",
+      category: "black",
+      src: "/4.jpeg",
     },
     {
-      img: "/5.jpeg",
+      category: "black",
+      src: "/5.jpeg",
     },
     {
-      img: "/6.jpeg",
+      category: "black",
+      src: "/6.jpeg",
     },
     {
-      img: "/7.jpeg",
+      category: "black",
+      src: "/7.jpeg",
     },
   ];
 
+  const [selectedCategory, setSelectedCategory] = useState("all");
+
+  const filteredImages = images1.filter((photo) => {
+    if (selectedCategory === "all") {
+      return true;
+    }
+    return photo.category === selectedCategory;
+  });
+
+  const features = [
+    "الشنطة مصنوعة من خامة المكروفيبر",
+    " الشنطة فيها 3 اماكن عشان تنظم متعلقاتكم اليومية",
+    "حزام الشنطة قابل للتوسيع يصل الي متر و ربع",
+    "مبطنة بالداخل بخامة PU واللون البرتقالي العصري",
+    "الوان الشنطة هتناسب جميع الاوقات",
+  ];
+
+  const [bigImage, setBigImage] = useState(images1[0]);
+
+  const handleSmallImageClick = (item) => {
+    setBigImage(item);
+  };
+
   return (
-    <section className="w-full mt-10">
+    <section className="w-full my-10">
       <div className="max-w-[90%] mx-auto grid gap-8 grid-cols-1 md:grid-cols-2 justify-center items-start">
         <div className="max-w-[600px] mx-auto grid gap-8 justify-center items-center">
-          <div className="  ">
+          <div className="">
             <Image
-              src="/1.jpeg"
+              src={bigImage.src}
               alt=""
               className="w-[800px] rounded-xl"
               width={800}
@@ -65,11 +119,12 @@ const Product = () => {
             />
           </div>
           <div className="grid w-full grid-cols-2 gap-5 overflow-hidden">
-            {data.map((item, id) => (
+            {filteredImages.map((item, id) => (
               <Image
                 alt=""
-                src={item.img}
-                className="w-full rounded-xl"
+                onClick={() => handleSmallImageClick(item)}
+                src={item.src}
+                className="w-full rounded-xl cursor-pointer"
                 width={500}
                 height={500}
                 key={id}
@@ -87,16 +142,40 @@ const Product = () => {
             <p>No Reviews Yet </p>
             <p className="font-bold text-xl">
               250 <span className="font-normal">EGP</span>{" "}
-              <span className="text-gray-400 font-normal">| 400</span>{" "}
+              <span className="text-gray-400 font-normal">
+                | <s>400</s>
+              </span>{" "}
               <span className="ml-3 font-normal text-sm py-1 px-2 bg-green-300 text-center rounded-full">
                 38%
               </span>
             </p>
             <div className="flex max-w-xl overflow-hidden flex-col mt-5">
-              <p className="font-bold ">color: Gray</p>
-              <p className="cursor-pointer my-3 text-center py-3 px-[80px] rounded-2xl border-2 w-fit bg-white border-black">
-                GRAY
+              <p className="font-bold ">
+                color:
+                {selectedCategory === "gray"
+                  ? "GRAY"
+                  : selectedCategory === "black"
+                  ? "BLACK"
+                  : "ALL"}
               </p>
+              <div className="flex w-full gap-3 ">
+                <p
+                  onClick={() => setSelectedCategory("gray")}
+                  className={`cursor-pointer my-3 text-center py-3 px-[30px] lg:px-[80px] rounded-2xl border-2 w-fit bg-white border-gray-300 ${
+                    selectedCategory === "gray" && "border-gray-900"
+                  }`}
+                >
+                  GRAY
+                </p>
+                <p
+                  onClick={() => setSelectedCategory("black")}
+                  className={`cursor-pointer my-3 text-center py-3 px-[30px] lg:px-[80px] rounded-2xl border-2 w-fit bg-white border-gray-300  ${
+                    selectedCategory === "black" && "border-gray-900"
+                  }`}
+                >
+                  BLACK
+                </p>
+              </div>
               <p className="font-bold mb-6">Time Left For Discont</p>
               <div className="flex w-full md:w-full mx-auto justify-center items-center text-center gap-[20px] md:gap-[30px] xl:gap-[60px]">
                 <div className="flex text-center text-sm md:text-lg justify-center items-center flex-col w-[18%] p-3 rounded-xl border-2 border-black">
@@ -118,17 +197,58 @@ const Product = () => {
                   Seconds
                 </div>
               </div>
-              <p className=" text-2xl text-center my-8 text-white bg-black py-3 rounded-xl">
-                اضغط هنا للشراء
-              </p>
+              <Link href="#contact">
+                <p className=" text-2xl text-center my-8 text-white bg-black py-3 rounded-xl ">
+                  اضغط هنا للشراء
+                </p>
+              </Link>
               <div className="flex w-full gap-8">
                 <p className="cursor-pointer flex gap-2">
-                  <Repeat2 className=" w-11 " /> Compare
+                  <Repeat2 className=" w-11" /> Compare
                 </p>
                 <p className="cursor-pointer flex gap-3">
                   <Share2 /> Share Product
                 </p>
-              </div>
+              </div>{" "}
+              <h2 className="flex w-full justify-end text-3xl mt-8 mb-6">
+                من هاي لاين Ultimate شنطه
+              </h2>
+              <p className="flex justify-end text-lg">
+                الشنطة فيها مميزات كتير هتسهل عليك يومك
+              </p>
+              {features.map((item, id) => (
+                <div
+                  className="flex felx-col w-full justify-end gap-2"
+                  key={id}
+                >
+                  <p className="flex text-center justify-center items-center gap-3 text-xl h-[70px]">
+                    {item}
+                    <svg
+                      className="w-8"
+                      xmlns="http://www.w3.org/2000/svg"
+                      x="0px"
+                      y="0px"
+                      width="100"
+                      height="100"
+                      viewBox="0 0 48 48"
+                    >
+                      <path
+                        fill="#c8e6c9"
+                        d="M44,24c0,11.045-8.955,20-20,20S4,35.045,4,24S12.955,4,24,4S44,12.955,44,24z"
+                      ></path>
+                      <path
+                        fill="#4caf50"
+                        d="M34.586,14.586l-13.57,13.586l-5.602-5.586l-2.828,2.828l8.434,8.414l16.395-16.414L34.586,14.586z"
+                      ></path>
+                    </svg>
+                  </p>
+                </div>
+              ))}
+              <p className="flex justify-end text-end text-[18px]">
+                :مقاس الشنطه <br /> العرض 31 سم <br /> الطول 16 سم <br /> طول
+                الحزام 125 سم قابل للتضيق
+              </p>
+              <Form />
             </div>
           </div>
         </div>
