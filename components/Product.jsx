0884d637f0a1c9fe184dafb8253d86a21e9a98ed
Beboo-stack <peteralber735@gmail.com>
2024-05-09 01:40:54 +0300

    z

--- components/Product.jsx
+++ components/Product.jsx
@@ -24,7 +24,7 @@ const Product = () => {
   useEffect(() => {
     const timer = setInterval(() => {
       const updateTimeLeft = getTimeLeft();
-      setTimeLeft(updateTimeLeft());
+      setTimeLeft(updateTimeLeft);
 
       if (
         updateTimeLeft.days === 0 &&
